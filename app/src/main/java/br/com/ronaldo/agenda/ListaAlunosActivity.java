dd4422d93126c3340943d666f838dd908b7a73fd
Ronaldo Pereira <ox99709179@gmail.com>
2017-11-15 23:23:20 -0200

    fazendo chamada

--- app/src/main/java/br/com/ronaldo/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/ronaldo/agenda/ListaAlunosActivity.java
@@ -1,8 +1,12 @@
 package br.com.ronaldo.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Browser;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -27,7 +31,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
+            //pega o aluno e sua posição e vai pro formulario
         listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override // quando clicado no nome
@@ -61,9 +65,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
     }
+
     @Override
     //atualiza a lista logo após gravar no banco
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         carregaLista();
     }
@@ -75,10 +80,29 @@ public class ListaAlunosActivity extends AppCompatActivity {
         //pega aluno selecionado
         final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
+        //opção chamada
+        MenuItem itemLigar = menu.add("Fazer ligação");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                //se permissão negada, pede pro usuario
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, 123);
+                }else{
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:"+aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+
         //opçao sms
         MenuItem itemSms = menu.add("enviar sms");
         Intent intentSms = new Intent(Intent.ACTION_VIEW);
-        intentSms.setData(Uri.parse("sms:"+aluno.getEndereco()));
+        intentSms.setData(Uri.parse("sms:"+aluno.getTelefone()));
         itemSms.setIntent(intentSms);
 
         //opçao mapa
@@ -91,7 +115,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         MenuItem itemSite = menu.add("Visitar site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
 
-        //acessar site
+        //acessar site, corrigir url
         String site = aluno.getSite();
         if (!site.startsWith("http://")){
             site = "http://"+site;
@@ -100,7 +124,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         intentSite.setData(Uri.parse(site));
         itemSite.setIntent(intentSite);
 
-        //opção deletar clicado
+        //opção deletar
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -116,8 +140,16 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+    }
+
+    @Override //resultado do pedido de permissao, tem as permissoes que foram pedidas e o resultado desses pedidos
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
+
+
 /*
 anotaçoes
 resolver arvore kantek abp 021017 20h00
